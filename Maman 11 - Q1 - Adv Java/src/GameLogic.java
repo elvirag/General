548a4e1f9fa8b0b97a0562ea772c9b599aba0dd8
Elvira Gandelman <elvirag@il.ibm.com>
2014-03-17 21:06:14 +0200

    Works!!! Yay!

--- Maman 11 - Q1 - Adv Java/src/GameLogic.java
+++ Maman 11 - Q1 - Adv Java/src/GameLogic.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class GameLogic{
 
-	
+
 	private static final int FIRST_DEAL = 2;
 	private static final int MAX_HAND_VALUE = 18;
 	private static final int BLACKJACK = 21;
@@ -11,54 +11,49 @@ public class GameLogic{
 	private DeckOfCards _gameDeck; 	//The Game Deck
 	private GameLogic _newGame;
 	public JOptionPane _pane = new JOptionPane();
-	
+
 	public GameLogic(){
 		_gameDeck = new DeckOfCards();
 		_gameDeck.shuffle(); // shuffle deck before starting to deal.
 		_playerHand = new Hand();
 		_houseHand = new Hand();
 	}
-	
+
 	public void beginPlay(){
 		JOptionPane.showMessageDialog(_pane, "Welcome to BlackJack!");
 		beginDeal();
 		hitHouse();
 		hitPlayer();
 	}
+
 	//deal two cards to both players
 	private void beginDeal(){
-		System.out.print("\nInside beginDeal...\n"); //TODO
 		for (int i = 0; i < FIRST_DEAL; i++){
-		_playerHand.addCard(_gameDeck.dealCard());
-		_houseHand.addCard(_gameDeck.dealCard());
+			_playerHand.addCard(_gameDeck.dealCard());
+			_houseHand.addCard(_gameDeck.dealCard());
 		}
 	}
-	
+
 	private void hitHouse(){
-		System.out.print("\nInside hitHouse...\n"); //TODO
 		while (_houseHand.getHandValue() < MAX_HAND_VALUE){
 			_houseHand.addCard(_gameDeck.dealCard());
-			showCards(); // to make sure that the last card didn't make us go over board and also if there is an ace, to fix it.
 		}
 	}
-	
-	public int showCards(){
-		int input = JOptionPane.showConfirmDialog(null, _playerHand.showHand() + "Would you like to draw another?", "Draw another card?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+	public int showCards(){ // only shows the cards of the player
+		int input = JOptionPane.showConfirmDialog(null, _playerHand.showHand() +
+				"Would you like to draw another?", "Draw another card?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 		return input;
 	}
-	
+
 	private void hitPlayer() {
-		if (showCards() == JOptionPane.YES_OPTION){
-			if (_playerHand.getHandValue() >= BLACKJACK)
-			{
-				int input = JOptionPane.showConfirmDialog(null, "Sorry, you have lost.\n Player: " + _playerHand.showHand() + "\nHouse: " + _houseHand.showHand(), "House wins!\n Would you like another game?", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE);
-				newGame(input);
-			}
+		while (showCards() == JOptionPane.YES_OPTION) {
+			_playerHand.addCard(_gameDeck.dealCard());
 		}
+		newGame(determineWinLose());
 	}
-	
+
 	public void newGame(int input) {
-		System.out.print("input = " + input + " yes: " + JOptionPane.YES_OPTION + " no: " + JOptionPane.NO_OPTION + "\n");
 		if (input == JOptionPane.NO_OPTION){
 			JOptionPane.showMessageDialog(null, "Thank you for playing!\nBye Bye!","Game Over", JOptionPane.PLAIN_MESSAGE);
 		}
@@ -66,22 +61,24 @@ public class GameLogic{
 			_newGame = new GameLogic();
 			_newGame.beginPlay();
 		}
-		
+
 	}
 
-	public void displayCards(){
-		//Don't think it's necessary. Maybe only to print it in a message.
+	public int determineWinLose(){ //when player is done taking cards, go here...
+
+		String result;
+		if (_playerHand.getHandValue() > _houseHand.getHandValue() && _playerHand.getHandValue() < BLACKJACK){ //win
+			result = " *** You have won the game! *** (^_^)\n";
+		}
+		else if (_playerHand.getHandValue() < _houseHand.getHandValue() && _houseHand.getHandValue() < BLACKJACK){ //lose
+			result = "You have lost. -_-\n";
+		}
+		else{ //tie
+			result = "It's a tie... o_O\n";
+		}
+		int input = JOptionPane.showConfirmDialog(null, "House: " + _houseHand.showHand() + "\nYou: " + _playerHand.showHand() + "\n\nThe resulf of the game is:\n" + result + "\n\nWould you like to play again?", "***Game Result***", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+		return input;
 	}
-	
-	//"hit the player" + decisions
-	
-	//display cards of both players
-	
-	//declare winner
-	
-	//decide whether to continue. 
-	
-	//when player decides to stop or passes 21, game is decided and a prompt appears.
-	
 	
 }
