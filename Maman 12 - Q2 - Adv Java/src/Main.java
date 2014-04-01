358871a9f86539ec3afcd50299dc6b4ac49aeb33
Elvira Gandelman <elvirag@il.ibm.com>
2014-04-01 20:24:38 +0300

    okay, almost all done, need to fix the null pointer, also note what I wrote

--- Maman 12 - Q2 - Adv Java/src/Main.java
+++ Maman 12 - Q2 - Adv Java/src/Main.java
@@ -1,19 +1,12 @@
-import javax.swing.JOptionPane;
+
 
 
 public class Main {
-
-	private static Integer input(){
-		return Integer.parseInt(JOptionPane.showInputDialog("What is the coordinate you want to input?\n(0-99 only!\n"));
-	}
 
 	public static void main(String[] args) {
 
 		MyPanel mypanel = new MyPanel();
 
-		while(true){
-			mypanel.addLine(input());
-		}
-
+			mypanel.init();
 	}
 }
--- Maman 12 - Q2 - Adv Java/src/MyPanel.java
+++ Maman 12 - Q2 - Adv Java/src/MyPanel.java
@@ -1,4 +1,10 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
+
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -7,42 +13,91 @@ import javax.swing.JPanel;
  * Repaints after the user has given an input to show the new added line.<br>
  */
 
-public class MyPanel extends JPanel{
+public class MyPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private ArrayList<Integer> lineArray = new ArrayList<Integer>();// the array list that holds all the line numbers
+	private ArrayList<Integer> lineArray = new ArrayList<Integer>();// the array
+	// list that
+	// holds all
+	// the line
+	// numbers
+	private int NUM_DOTS = 10;
+	private int SPACE = 20;
 
-	public MyPanel(){
+	public MyPanel() {
 		super();
-	} //empty builder only to show the panel
+	} // empty builder only to show the panel
 
-	Object input = JOptionPane.showInputDialog("What is the coordinate you want to input?\n(0-99 only!\n");
-	String str = (String)input;
-	Integer integer;
+	public void init() {
+		regFrame();
+		while(true){
+			getPoint();
+		}
 
-	try 
-	{
-		Integer.parseInt(str);
-	}
-	catch(NumberFormatException nme)
-	{
-		JOptionPane.showMessageDialog(null, "You have chosen to stop drawing lines.\nThank you for using my line painter!\n", "Bye - Bye!", JOptionPane.NO_OPTION);
 	}
 
-	if (input != null) //this is for the option that the user clicks cancel.
-	{
-		addLine(integer);
+	public void regFrame() {
+		JFrame frame = new JFrame("Testing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(NUM_DOTS * SPACE, NUM_DOTS * SPACE);
+		frame.add(new MyPanel());
+		frame.setVisible(true);
 	}
 
-	public void addLine(Integer point){
-		if (point >= 0 && point <= 99)
+	public void paintComponent(Graphics g) {
+		// RecoverGraphics2D
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g.setColor(Color.BLACK);
+
+		// Ellipse2D.Double is an inner class of Ellipse2D.
+		for (int i = 0; i <= NUM_DOTS * SPACE; i += SPACE)
+			for (int j = 0; j <= NUM_DOTS * SPACE; j += SPACE) {
+				g2.draw(new Ellipse2D.Double(i, j, 1, 1));
+			}
+		if (lineArray.size() > 1) {
+			for (int i = 1; i < lineArray.size(); i++) {
+				g2.drawLine(lineArray.get(i - 1) / 10,
+						lineArray.get(i - 1) % 10, lineArray.get(i) / 10,
+						lineArray.get(i) % 10);
+			}
+		}
+
+	}
+
+	private void addLine(Integer input) {
+		if (input >= 0 && input <= 99)
 		{
-			lineArray.add(point);
+			lineArray.add(input);
 			repaint();
 		}
-
 		else{
-			JOptionPane.showMessageDialog(null, "You have input a number in the wrong range. Please try again.", "Wrong input", JOptionPane.WARNING_MESSAGE);
+			JOptionPane.showMessageDialog(null,
+					"You have input a number in the wrong range. Please try again.",
+					"Wrong input", JOptionPane.WARNING_MESSAGE);
 		}
+	}
+
+	private void getPoint(){
+		Object input = JOptionPane.showInputDialog("What is the coordinate you want to input?\n(0-99 only!\n");
+		String str = (String)input;
+		Integer integer = null;
+
+		try
+		{
+			Integer.parseInt(str);
+		}
+		catch(NumberFormatException nme)
+		{
+			JOptionPane.showMessageDialog(null,
+					"You have chosen to stop drawing lines.\nThank you for using my line painter!\n",
+					"Bye - Bye!", JOptionPane.NO_OPTION);
+		}
+
+		if (input != null) //this is for the option that the user clicks cancel.
+		{
+			addLine(integer);
+		}
+
 	}
 }
