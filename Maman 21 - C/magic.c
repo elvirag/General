c7cdefa67d876462365fbca58972f5e540e6f4ce
Elvira Gandelman <elvirag@gmail.com>
2014-04-13 02:27:59 +0300

    Now MaMan 21 is almost ready. Only need to check the chars thing.

--- Maman 21 - C/magic.c
+++ Maman 21 - C/magic.c
@@ -1,23 +1,27 @@
 /*
  * magic.c
  *
- *  Created on: 31 ???? 2014
+ *  Created on: 1 April 2014
  *      Author: elvirag
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 5 /**/
+#define N 3 /*Defining the dimension of the magic square*/
+
 typedef enum {
-	FALSE = 0, TRUE
+	FALSE = 0, TRUE = 1
 } boolean;
 
 /*All my functions declarations*/
-void input(); /**/
-void print(int * magic); /**/
-boolean test_cube(int * magic); /**/
+int * input(); /*The function that gets the input*/
+void print(int * magic); /*The function that prints*/
+boolean test_cube(int * magic); /*The function that tests the magic!*/
 
+/*
+ * Printing the matrix
+ * */
 void print(int * magic) {
 	int row, col;
 
@@ -28,15 +32,37 @@ void print(int * magic) {
 	}
 }
 
-//void input() {
-//	int row, col;
-//
-//	for (row = 0; row < N; row++) {
-//		for (col = 0; col < N; col++)
-//			scanff("%c", &(magic + row * N + col));
-//		printf("\n");
-//	}
-//}
+/*Scanning the input*/
+int * input() {
+	char garbage;
+	int row, col;
+	int counter = 0;/*Counts number of parameters scanned so I won't have to hit Enter each time*/
+	int * magic = calloc(N * N, sizeof(int)); /*Initializing the array with zeroes*/
+
+	for (row = 0; row < N; row++)
+		for (col = 0; col < N; col++)
+			counter += scanf("%d", (magic + row * N + col)); /*Counting my parameters*/
+
+	/*This portion of code is needed so the numbers scanned won't just go to the next magic square.*/
+	scanf("%c", &garbage);
+	if (garbage != '\n')
+		counter++;
+
+	/*In case of too many parameters*/
+	if (counter > N * N) {
+		printf(
+				"You have inputed too many parameters.\nThe program will now quit.\n");
+		exit(0);
+	}
+	/*In case of too less parameters*/
+	if (counter < N * N) {
+		printf(
+				"You have inputed too few parameters.\nThe program will now quit.\n");
+		exit(0);
+	}
+
+	return magic;
+}
 
 /*Well, I can test the sums of the rows and columns right after one run of the for loop,
  * but the diagonals require to run all over the matrix first.*/
@@ -47,7 +73,6 @@ boolean test_cube(int * magic) {
 	for (row = 0; row < N; row++) {
 		sum_row = 0, sum_col = 0;
 		for (col = 0; col < N; col++) {
-//			printf("\nmagic[%d][%d] = %d", row, col, *(magic + row * N + col)); TODO
 			sum_row += *(magic + row * N + col); /*sums the rows, one at a time*/
 			sum_col += *(magic + row + col * N); /*sums the columns, one at a time*/
 			if (row == col) /*sums the regular diagonal*/
@@ -56,17 +81,10 @@ boolean test_cube(int * magic) {
 				sum_diag2 += *(magic + row * N + col);
 		}
 
-//		printf("sum_row = %d\n", sum_row);TODO
-//		printf("sum_col = %d\n", sum_col);
 		if (sum_row != sum_col)
 			return FALSE;
 
 	}
-
-//	printf("sum_row = %d\n", sum_row);TODO
-//	printf("sum_col = %d\n", sum_col);
-//	printf("sum_diag1 = %d\n", sum_diag1);
-//	printf("sum_diag2 = %d\n", sum_diag2);
 
 	if (sum_col != sum_diag1 || sum_diag1 != sum_diag2)
 		return FALSE;
@@ -75,16 +93,18 @@ boolean test_cube(int * magic) {
 }
 
 int main() {
+	int *p = NULL;
 
-	int magic[N][N] = { { 15, 8, 1, 24, 17 }, { 16, 14, 7, 5, 23 }, { 22, 20,
-			13, 6, 4 }, { 3, 21, 19, 12, 10 }, { 9, 2, 25, 18, 11 } };
+	do {
+		print(p = input());
 
-	//input();
-	print(*(magic));
-	if (test_cube(*(magic))) {
-		printf("\nThis cube is indeed magic!!!");
-	} else
-		printf("\nThis cube is NOT magic!!!"); /*add some regex magic here*/
-	//free(magic);
-	return 0;
+		if (test_cube(p))
+			printf("\nThis cube is indeed magic!!!\n\n");
+		else
+			printf("\nThis cube is NOT magic!!!\n\n");
+
+		p = NULL;
+	} while (1);
+
 }
+
