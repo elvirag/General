c5bea205e751e60aff31d7ec203e93864da4f155
Elvira Gandelman <elvirag@gmail.com>
2014-04-26 21:15:58 +0300

    After some fixes & reworking - now checks every number if fits the range and not in the end when I'm done typing Thanks, satyr!

--- Maman 21 - C/magic.c
+++ Maman 21 - C/magic.c
@@ -2,37 +2,68 @@
  * magic.c
  *
  *  Created on: 1 April 2014
- *      Author: elvirag
+ *      Author: Elvira Gandelman
+ *      ID:     307032623
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 2 /*Defining the dimension of the magic square*/
+#define N 3 /*Defining the dimension of the magic square*/
 
 typedef enum {
 	FALSE = 0, TRUE = 1
 } boolean;
 
 /*All my functions declarations*/
-int * input(); /*The function that gets the input*/
-boolean check_input(int * magic); /*The function that gets the input*/
+int * input(); /*The function that gets the input and checks it*/
 void print(int * magic); /*The function that prints*/
 boolean test_cube(int * magic); /*The function that tests the magic!*/
-
-
+void prog_exit();/*So I don't have to repeat myself in the strings.*/
 
 /*Scanning the input*/
 int * input() {
 	char garbage;
 	int row, col;
-	int counter = 0;/*Counts number of parameters scanned so I won't have to hit Enter each time*/
+	int member = 0; /*Counts the members of the array so I can know we have it all filled*/
+	int counter = 0, scans = 0;/*Counts number of parameters scanned & parameters successfully scanned*/
 	int * magic = calloc(N * N, sizeof(int)); /*Initializing the array with zeroes*/
+	boolean * check_array = calloc(N * N, sizeof(int)); /*Initializing the array with zeroes*/
 
 	for (row = 0; row < N; row++)
-		for (col = 0; col < N; col++)
+		for (col = 0; col < N; col++) {
 			counter += scanf("%d", (magic + row * N + col)); /*Counting my parameters*/
+			scans++;
+			if (scans != counter) {
+				printf(
+						"You have input some characters that are not integers!\n");
+				prog_exit();
+				return NULL;
+			} else if (1
+					> *(magic + row * N + col)|| *(magic + row * N + col) > N * N) {
+				printf("%d is not in the range specified.",
+						*(magic + row * N + col));
+				prog_exit();
+				return NULL;
 
+			} else if (*(check_array + *(magic + row * N + col) - 1) == TRUE) {
+				printf("\nThis member (%d) already exists in the cube,"
+						"\nPlease don't repeat numbers in the magic cube!\n",
+						*(magic + row * N + col));
+				prog_exit();
+				return NULL;
+			} else {
+				*(check_array + *(magic + row * N + col) - 1) = TRUE;
+				member++;
+			}
+		}
+	if (member != N * N){
+		printf(
+				/*Not supposed to get here. If I have all the length of legal characters within the range and no thing repeats, this is not logical that this sequence may exist!*/
+				"Some of the numbers in the sequence needed (1 - N^2) are missing.\nThe program will now exit.\n");
+		prog_exit();
+		return NULL;
+	}
 	/*This portion of code is needed so the numbers scanned won't just go to the next magic square.*/
 	scanf("%c", &garbage);
 	if (garbage != '\n')
@@ -40,52 +71,19 @@ int * input() {
 
 	/*In case of too many parameters*/
 	if (counter > N * N) {
-		printf(
-				"You have inputed too many parameters.\nThe program will now quit.\n");
-		exit(0);
+		printf("You have inputed too many parameters.\n");
+		prog_exit();
+		return NULL;
 	}
 	/*In case of too less parameters*/
 	if (counter < N * N) {
-		printf(
-				"You have inputed too few parameters.\nThe program will now quit.\n");
-		exit(0);
+		printf("You have inputed too few parameters.\n");
+		prog_exit();
+		return NULL;
 	}
-	if (check_input(magic))
-		return magic;
-	else{ /*Not supposed t even get here.*/
-		printf("\nSorry, the input is wrong.\nThe program will now quit");
-		exit(0);
-	}
+	return magic;
 }
 
-boolean check_input(int * magic){
-	int row, col, member = 0;
-	boolean * check_array = calloc(N * N, sizeof(int)); /*Initializing the array with zeroes*/;
-
-	for (row = 0; row < N; row++)
-		for (col = 0; col < N; col++){
-			if( 0 >= *(magic + row * N + col) || *(magic + row * N + col) > N*N){
-				printf("%d is not in the range specified", *(magic + row * N + col));
-				return FALSE;
-			}
-			else if (*(check_array + *(magic + row * N + col) -1) == TRUE){
-				printf("\nThis member (%d) already exists in the cube,\nPlease don't repeat numbers in the magic cube!\n\nThe program will now quit.\n",*(magic + row * N + col));
-				exit(0);
-			}
-			else *(check_array + *(magic + row * N + col) -1) = TRUE;
-			member++;
-			printf("\ncheck_array[%d]=%d\n",*(magic + row * N + col) -1,*(check_array + row * N + col));
-		}
-	if (member == N*N){
-		printf("member = %d",member);/*TODO remove*/
-		return TRUE;
-	}
-	else
-	{
-		printf("Some of the numbers in the sequence needed (1 - N^2) are missing.\nThe program will now exit.\n");
-		exit(0);
-	}
-}
 
 
 /*
@@ -100,7 +98,6 @@ void print(int * magic) {
 		printf("\n");
 	}
 }
-
 
 /*Well, I can test the sums of the rows and columns right after one run of the for loop,
  * but the diagonals require to run all over the matrix first.*/
@@ -130,13 +127,18 @@ boolean test_cube(int * magic) {
 	return TRUE;
 }
 
+void prog_exit() {
+	printf("\nProgram will now quit.\n");
+	exit(0);
+}
+
 int main() {
 	int *p = NULL;
 
 	do {
 		print(p = input());
 
-		if (test_cube(p) )
+		if (test_cube(p))
 			printf("\nThis cube is indeed magic!!!\n\n");
 		else
 			printf("\nThis cube is NOT magic!!!\n\n");
