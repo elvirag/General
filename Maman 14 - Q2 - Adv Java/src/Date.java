ae903666915ec6e8fa98063b1993ef1145fd936d
Elvira Gandelman <elvirag@gmail.com>
2014-05-06 20:54:16 +0300

    starting Q2... going to first start something that I need to do earlier.

---
+++ Maman 14 - Q2 - Adv Java/src/Date.java
@@ -0,0 +1,32 @@
+
+public class Date {
+
+	private int _day;
+	private int _month;
+	private int _year;
+	
+	
+	//GETTERS
+	public int getDay() {
+		return _day;
+	}
+	public int getMonth() {
+		return _month;
+	}
+	public int getYear() {
+		return _year;
+	}
+	
+	//SETTERS
+	public void set_day(int day) {
+		_day = day;
+	}
+	public void set_month(int month) {
+		_month = month;
+	}
+	public void set_year(int year) {
+		_year = year;
+	}
+	
+	
+}
