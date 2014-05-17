10af0641fee9f50712005dde6592b33a1c2266ae
Elvira Gandelman <elvirag@gmail.com>
2014-05-17 18:59:36 +0300

    some stuff work... *fingers cross*

--- Maman 13 - Q1 - Adv Java/src/ActionsPanel.java
+++ Maman 13 - Q1 - Adv Java/src/ActionsPanel.java
@@ -1,9 +1,10 @@
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
@@ -12,6 +13,10 @@ public class ActionsPanel extends JPanel implements MouseListener{
 	 * 
 	 */
 	private static final long serialVersionUID = -969652273342908891L;
+	
+	private Robot _boardRobot;
+	private JButton _selectedRobot;
+	
 	protected JButton move;
 	protected JButton right;
 	protected JButton left;
@@ -39,31 +44,131 @@ public class ActionsPanel extends JPanel implements MouseListener{
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println("click3");
+		System.out.println("click-action\n");
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		System.out.println("enter3");
-	}
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
 
 
 	@Override
-	public void mouseExited(MouseEvent e) {
-		System.out.println("exit3");
-	}
+	public void mousePressed(MouseEvent e) {}
 
 
 	@Override
-	public void mousePressed(MouseEvent e) {
-		System.out.println("press3");
-	}
-
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		System.out.println("release3");
-	}
+	public void mouseReleased(MouseEvent e) {}
 
 }
+
+
+
+//
+//@Override
+//public void mouseClicked(MouseEvent e) {
+//	System.out.println("click-total-panel: " + ((JButton) e.getComponent()).getText() + "\n");
+//	
+//	if (((JButton) e.getComponent()).getText().isEmpty()){
+//		//here writing to add robot
+//		System.out.println("adding a robot!!!");
+//		
+//		Direction direct = null;
+//		String direcString = null;
+//		_selectedRobot = (JButton) e.getComponent();
+//		do {
+//			direcString = JOptionPane.showInputDialog("Please input direction of robot: (Up, Down, Right or Left only)");
+//			switch(direcString){
+//			case "up":
+//				direct = Direction.UP;
+//				break;
+//			case "down":
+//				direct = Direction.DOWN;
+//				break;
+//			case "right":
+//				direct = Direction.RIGHT;
+//				break;
+//			case "left":
+//				direct = Direction.LEFT;
+//				break;
+//			default:
+//				setBackground(Color.RED);
+//			}
+//		}
+//		while (direct == null);
+//
+//		_boardRobot = new Robot(direct);
+//		_selectedRobot.setText(_boardRobot.toString());
+//	}
+//	
+//	else if (((JButton) e.getComponent()).getText().matches("\\d")){
+//		//here writing to store this robot and change it's background so I can use it to do things.
+//		//also, if another robot was selected, the one in the selection loses it's background first!
+//		System.out.println("Clicking an existing robot!!!");
+//	}
+//	
+//	else if (((JButton) e.getComponent()).getText().matches("\\w")){
+//		//here writing to do some action about chosen robot
+//		//don't forget to add something if referenced robot is null
+//		System.out.println("Clicking an action button!!!");
+//	}
+//}
+
+
+//}
+//if (e.getComponent().getName().equalsIgnoreCase("Move") ){
+//	System.out.println("Move!");
+//}
+//if (e.getComponent().getName().equalsIgnoreCase("Turn Right") ){
+//	System.out.println("Turn Right!");
+//}
+//if (e.getComponent().getName().equalsIgnoreCase("Turn Left") ){
+//	System.out.println("Turn Left!");
+//}
+//if (e.getComponent().getName().equalsIgnoreCase("Delete") ){
+//	System.out.println("Delete!");
+//}
+//}
+//
+//
+//@Override
+//public void mouseClicked(MouseEvent arg0) {
+//
+//if (_robot == null){
+//
+//	Direction direct = null;
+//	String direcString = null;
+//	do {
+//		direcString = JOptionPane.showInputDialog("Please input direction of robot: (Up, Down, Right or Left only)");
+//		switch(direcString){
+//		case "up":
+//			direct = Direction.UP;
+//			break;
+//		case "down":
+//			direct = Direction.DOWN;
+//			break;
+//		case "right":
+//			direct = Direction.RIGHT;
+//			break;
+//		case "left":
+//			direct = Direction.LEFT;
+//			break;
+//		default:
+//			setBackground(Color.RED);
+//		}
+//	}
+//	while (direct == null);
+//
+//	_robot = new Robot(direct);
+//	this.setText(_robot.toString());
+//}
+//
+//else{
+//	this.setBackground(Color.PINK);
+//	_robot.getIdRobot();
+//
+//}
+//}
+
--- Maman 13 - Q1 - Adv Java/src/WorldPanel.java
+++ Maman 13 - Q1 - Adv Java/src/WorldPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
@@ -40,31 +41,58 @@ public class WorldPanel extends JPanel implements MouseListener{
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		//TODO to get the id of the robot for another method to output it.
-		System.out.println("click");	
+		System.out.println("click-world\n");
+		JButton currRobot = (JButton) e.getComponent();
+		
+		if (currRobot.getText().isEmpty()){
+			System.out.println("adding a robot!!!");
+			
+			Direction direct = null;
+			String direcString = null;
+			String robotPrompt = "Please input direction of robot:\n(Up, Down, Right or Left only)";
+			String robotErrorPromt = "Wrong input. " + robotPrompt;
+
+			do {
+				direcString = JOptionPane.showInputDialog(robotPrompt);
+				switch(direcString){
+				case "up":
+					direct = Direction.UP;
+					break;
+				case "down":
+					direct = Direction.DOWN;
+					break;
+				case "right":
+					direct = Direction.RIGHT;
+					break;
+				case "left":
+					direct = Direction.LEFT;
+					break;
+				default:
+					robotPrompt = robotErrorPromt;
+				}
+			}
+			while (direct == null);
+	
+			Robot thisRobot = new Robot(direct);
+			_robotsWorld.addRobot(thisRobot, getLocation());
+			System.out.println("here:" + _robotsWorld.getRobot(getLocation()));
+			currRobot.setText(thisRobot.toString());
+		}
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		System.out.println(e.getSource() + " a Jbutton");
-		System.out.println("enter2");
-	}
+	public void mouseEntered(MouseEvent e) {}
 
 
 	@Override
-	public void mouseExited(MouseEvent e) {
-		System.out.println("exit2");
-	}
+	public void mouseExited(MouseEvent e) {}
 
 
 	@Override
-	public void mousePressed(MouseEvent e) {
-		System.out.println("press");
-	}
+	public void mousePressed(MouseEvent e) {}
 
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		System.out.println("release");
-	}
+	public void mouseReleased(MouseEvent e) {}
 
 }
