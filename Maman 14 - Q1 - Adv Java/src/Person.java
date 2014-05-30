bfac63f01133455a9bae5256b91ab8cdeb5e6e90
Elvira Gandelman <elvirag@il.ibm.com>
2014-05-30 19:08:44 +0300

    Q1 of Maman 14 doced. WEeeeeeeeeeeeeeeeeeeee all done. Now submitting.

--- Maman 14 - Q1 - Adv Java/src/Person.java
+++ Maman 14 - Q1 - Adv Java/src/Person.java
@@ -1,42 +1,52 @@
-
+/**
+ * The person class.
+ * Each person has a name and a birth year.
+ * It sets the dates, checks if birth year is legal, and sets the person.
+ */
 public class Person implements Comparable<Person>{
 
-	private String _name;
-	private Integer _birthYear;
+	private String _name; //the name of person
+	private Integer _birthYear; //the birth year
 	final int MAX_BIRTH_RANGE = 114; //The gap
 	final int MIN_BIRTH_DATE = 1900; //The oldest person
-	private String name = "Person ";
-	private static int number = 1;
-	
+	private String name = "Person "; //The name of the person
+	private static int number = 1; //The counter applied to name people...
+
+	/**
+	 * The constructor. Generates the person object, with an unique name
+	 * @param birthYear is the birth year of the person
+	 */
 	public Person(Integer birthYear){
-		_name = name + number;
-		number++;
-		setBirthYear(birthYear);
+		_name = name + number; //name is created and set
+		number++; //number of people is incremented
+		setBirthYear(birthYear); //birth year is set
 	}
 
-	private void setBirthYear(Integer birthYear) {
-		if (birthYear < MIN_BIRTH_DATE){
+	private void setBirthYear(Integer birthYear) { //sets birth year after verification
+		if (birthYear < MIN_BIRTH_DATE){ //if birth year is not, resetting to minimal year
 			System.out.println("The person \"" + _name + "\" is too old. Resetting to " + MIN_BIRTH_DATE + ".");
 			_birthYear = MIN_BIRTH_DATE;
 		}
 
-		else if (birthYear > MIN_BIRTH_DATE + MAX_BIRTH_RANGE){
+		else if (birthYear > MIN_BIRTH_DATE + MAX_BIRTH_RANGE){ //if birth year is too advanced (we don't have anyone born in 2015 yet.)
 			System.out.println("The year you have written doesn't exist yet. Resetting to " + (MIN_BIRTH_DATE + MAX_BIRTH_RANGE) + ".");
-			_birthYear = MIN_BIRTH_DATE + MAX_BIRTH_RANGE;
+			_birthYear = MIN_BIRTH_DATE + MAX_BIRTH_RANGE; //set to maximum year.
 		}
 
-		else _birthYear = birthYear;
+		else _birthYear = birthYear; //or just set the legal one
 	}
 
+	/**
+	 * Gets the birth year
+	 * @return birth year
+	 */
 	public Integer getBirthYear(){
 		return _birthYear;
 	}
 
-	@Override
-	public String toString(){
-		return "( " +_name + "; " + _birthYear + " )";
-	}
-
+	/**
+	 * Compares people by birth dates
+	 */
 	@Override
 	public int compareTo(Person person) {
 		if (this.getBirthYear() < person.getBirthYear())
@@ -45,5 +55,14 @@ public class Person implements Comparable<Person>{
 			return 1;
 		else
 			return 0;
+	}
+	
+	/**
+	 * The toString method.
+	 * Return the name and birth year of a person
+	 */
+	@Override
+	public String toString(){
+		return "( " +_name + "; " + _birthYear + " )";
 	}
 }
--- Maman 14 - Q1 - Adv Java/src/SortedPair.java
+++ Maman 14 - Q1 - Adv Java/src/SortedPair.java
@@ -1,39 +1,43 @@
-
+/**
+ * the class that allows us to create all sorts of sorted pairs.
+ * Gets two objects of the same type and returns the pair sorted.
+ * @param <T> - the type of object the pair orders.
+ */
 public class SortedPair<T extends Comparable<T>>{
 
-	private T _element1;
-	private T _element2;
+	private T _element1; //one of the elements
+	private T _element2; //the second element
 
+	/**
+	 * The constructor. It sorts the pair.
+	 * @param element1 - the first element in the pair (from user)
+	 * @param element2 - the second element in the pair (from user)
+	 * @throws IllegalPair
+	 */
 	public SortedPair(T element1, T element2) throws IllegalPair{
-		int result = 0;
-		result = element1.compareTo(element2);
+		int result = 0; //an int to compare the elements
+		result = element1.compareTo(element2); //the result of the compare
 
 
-		if (result < 0){
-			_element1 = element1;
-			_element2 = element2;
+		if (result < 0){ // if the first is smaller that the second, it is the first indeed.
+			_element1 = element1; //putting the first as the first.
+			_element2 = element2; //putting the second as the second.
 		}
-		else if (result > 0){
-			_element1 = element2;
-			_element2 = element1;
+		else if (result > 0){ // if the first is smaller that the second, it is the second.
+			_element1 = element2; //putting the first as the second.
+			_element2 = element1; //putting the second as the first.
 		}
 		else
-			try{
-				throw new IllegalPair();
-			}
-		catch(IllegalPair ip){
-			throw new IllegalPair();
-		}
+			throw new IllegalPair(); //if not, throws the exception
 	}
 
-	public T getFirst(){
-		return _element1;
-	}
+	//GETTERS
+	public T getFirst(){return _element1;} //returns the first element
+	public T getSecond(){return _element2;} //returns the second element
 
-	public T getSecond(){
-		return _element2;
-	}
-
+	/**
+	 * The toString of the class. Returns the pair as needed.
+	 */
 	@Override
 	public String toString(){
 		return "< " + _element1 + ", " + _element2 + " >";
